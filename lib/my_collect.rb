9057972b107367eb625d2696eebbdf99eb381c3c
Nash Brancic <nashfft@gmail.com>
2019-02-06 02:14:43 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,8 @@
 def my_collect(array)
   i = 0
-  new_collection= []
+  collection= []
   while i < array.length
-    new_collection << yield(collection[i])
+    collection << yield(collection[i])
     i += 1
   end
   new_collection
